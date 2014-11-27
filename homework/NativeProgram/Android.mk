b7224098e08a1cd81ce670e6c1e113b781bfc0bf
machenxi <1304085629@qq.com>
2014-11-27 17:48:09 +0800

    first homework

---
+++ homework/NativeProgram/Android.mk
@@ -0,0 +1,7 @@
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES:= android5.c
+LOCAL_MODULE := android5
+include $(BUILD_EXECUTABLE)
+
---
+++ homework/NativeProgram/android5.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+  printf("*Hello World!!!*\n");
+  printf("Group: Android 5")
+  return 0;
+}
