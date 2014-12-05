f4fb2cd31c8e2334ed1db1aa306cf18fbfc86410
weiwei-bjtu <ivy650255@gmail.com>
2014-12-05 14:06:35 +0800

    2nd android homework of group 5
    Signed-off-by: weiwei-bjtu <ivy650255@gmail.com>

---
+++ homework/NativeSercive/HelloWorldClient/HelloWorld.h
@@ -0,0 +1,13 @@
+#ifndef ANDROID_HELLOWORLD_H  
+#define ANDROID_HELLOWORLD_H  
+
+namespace android  
+{  
+    class HelloWorld {  
+    public:  
+        void printResult(String8 name);  
+        private:  
+        static const void getHelloWorldService();  
+    };  
+}; //namespace   
+#endif 
